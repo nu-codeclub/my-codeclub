f65f1e1d328c8563a54d3baf7715cdd7885a4387
Nupoor Chavan <nupoor@cloudflare.com>
2022-11-12 09:40:16 +0000

    Add index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 export default {
-	fetch() {
+	fetch(request) {
+		if(request.method=="POST") {
 		return new Response('Hello worker!', {
 			headers: {
 				'content-type': 'text/plain',
 			},
 		});
+	}
+	else {
+		return new Response('Error Worker!', {
+			headers: {
+				'content-type' : 'text/plain',
+			}
+		})
+	}
 	},
 };
